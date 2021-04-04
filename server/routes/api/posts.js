d3853f34495e8b73352b4c07ef40fedd8cc722f3
Georgi <g.nikolov@next-it.bg>
2021-04-04 13:27:36 +0300

    first commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router();
+
+// Get Posts
+router.get("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// Add Posts
+router.post("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
+
+// Delete Posts
+router.delete('/:id', async (req, res ) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({
+    _id: new mongodb.ObjectID(req.params.id)
+  })
+  res.status(200).send();
+})
+
+
+async function loadPostCollection() {
+  const MongoClient = require('mongodb').MongoClient;
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://asd123:asd123@cluster0.s8g3s.mongodb.net/vue_express?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,useUnifiedTopology: true
+    }
+  );
+  
+  return client.db('vue_express').collection('vue_express');
+}
+
+module.exports = router;
+
+
