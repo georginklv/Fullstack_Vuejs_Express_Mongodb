ba42b81d8e2d90fad5b2aa9cf58a070df8dec596
Georgi <g.nikolov@next-it.bg>
2021-04-05 11:25:04 +0300

    asd

--- server/index.js
+++ server/index.js
@@ -20,6 +20,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
